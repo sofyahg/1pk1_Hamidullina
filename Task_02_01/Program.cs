a9bdf05f88b2f746cc9eb1a5a04f104eedf68be0
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-01-23 23:18:09 +0500

    задание 1

---
+++ Task_02_01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_02_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
