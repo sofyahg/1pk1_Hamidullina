5e62ed64ab0fcdc102fdc97fb19beae83175591b
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-01-27 08:11:35 +0500

    9 задание выполнено

---
+++ Task_04_09/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_04_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] mas = new int[20];        // массив случайных чисел
+            int[] uniqMas = new int[20];    // массив уникальных случайных чисел
+
+            Console.WriteLine("Уникальные элементы массива:");
+
+            bool elemIsZero = true;     // елем. является ли 0
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rnd.Next(0, 10);
+
+                /* если элем. 0 выводим его отдельно 
+                 * тк массив uniqMas по умолчанию заполнен нулями 
+                 * и поиск 0 в нём невозможен */
+                if (mas[i] == 0 && elemIsZero == true)
+                {
+                    elemIsZero = false;
+                    Console.Write("0 ");
+                }
+
+                // если элем. нет в массиве uniqMas добавить его туда и вывести на консоль
+                if (Array.IndexOf(uniqMas, mas[i]) == -1)
+                {
+                    uniqMas[i] = mas[i];
+                    Console.Write($"{mas[i]} ");
+                }
+            }
+            }
+    }
+}
