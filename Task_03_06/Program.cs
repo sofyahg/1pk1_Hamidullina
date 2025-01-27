c85a77e0dcfbf2f31a864c211f61045107e4f9e9
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-01-27 07:32:20 +0500

    выполнено 6 задание 3 лекции

---
+++ Task_03_06/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_03_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, которая выводит таблицу значений функции: 𝑦=|𝑥|для -4≤x≤4, с шагом h = 0,5.
+            Console.WriteLine("Таблица значений функции y = |x|:");
+            Console.WriteLine("xt|y|");
+
+            for (double x = -4; x <= 4; x += 0.5)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"{x} t {y}");
+            }
+        }
+    }
+}
