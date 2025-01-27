b3a5190ad692fbe57a4979e4ab267cb0e3021b97
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-01-27 07:16:53 +0500

    Выполнено 4 задание 3 лекции

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Пользователь вводить в консоли произвольный текст. После каждого ввода консоль очищается.Когда
+            //пользователь вводить слово «exit» или пустую строку – ввод останавливается и выводиться количество строк
+            //введенных пользователем.
+            int lineCount = 0;
+            string input;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Введите текст (для выхода введите 'exit' или оставьте строку пустой):");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input) && input.ToLower() != "exit")
+                {
+                    lineCount++;
+                }
+
+            } while (!string.IsNullOrEmpty(input) && input.ToLower() != "exit");
+
+            Console.WriteLine($"Количество введенных строк: {lineCount}");
+        }
+    }
+}
