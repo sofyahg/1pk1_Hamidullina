00466fb32605596dad07ab30f7e6264c27e9f9b3
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-01-27 07:41:22 +0500

    9 задание 3 лекции выполнено

---
+++ Task_03_09/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_03_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100; // начальная сумма вклада
+            int p = 10;  // процент увеличения вклада ежегодно
+            int y = 200; // целевая сумма вклада
+
+            int years = 0; // количество лет
+
+            while (x < y)
+            {
+                x += (x * p) / 100; // увеличиваем вклад на p процентов
+                years++;
+            }
+
+            Console.WriteLine(years);
+        }
+    }
+}
