65f8d406fbec0374358e34b92041cf1761136e3a
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-01-27 08:08:00 +0500

    выполнено 7 задание 4 лекции

---
+++ Task_04_07/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_04_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+         В массиве на 30 элементов содержатся данные по росту учеников в классе. Рост мальчиков условно задан
+         отрицательными значениями. Вычислить и вывести количество мальчиков и девочек в классе и средний рост для
+         мальчиков и девочек. При выводе избавиться от отрицательных значений.*/
+
+            {
+                Random rnd = new Random();
+                int[] height = new int[30];
+
+                // количества мальчиков, девочек и их суммарный рост
+                int countBoy = 0;
+                double summHeightBoy = 0;
+                int countGirl = 0;
+                double summHeightGirl = 0;
+
+                for (int i = 0; i < height.Length; i++)
+                {
+                    height[i] = rnd.Next(155, 186) * (rnd.Next(2) * 2 - 1); // рандомно от 155 до 186 и от -155 до -186
+
+                    // подсчёт детей и их суммарного роста
+                    if (height[i] < 0)
+                    {
+                        summHeightBoy += Math.Abs(height[i]);
+                        countBoy++;
+                    }
+                    else
+                    {
+                        summHeightGirl += height[i];
+                        countGirl++;
+                    }
+                }
+                Console.WriteLine($"Девочек в классе: {countGirl}, мальчиков: {countBoy}.");
+                Console.WriteLine("Ср. рост девочек: {0:g6}, ср. рост мальчиков: {1:g6}", summHeightGirl / countGirl, summHeightBoy / countBoy);
+            }
+        }
+    }
+}
