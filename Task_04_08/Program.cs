b45c6035301018766b13470bd1e27cef5525397e
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-01-27 08:09:54 +0500

    8 задание выполнено

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_04_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] mas = new int[50];    // массив рандомных чисел
+
+            // заполнение массива числами
+            for (int i = 0; i < mas.Length; i++)
+                mas[i] = rnd.Next(0, 10);
+
+            // проверка на одинаковые пары
+            int count = 0;
+            for (int i = 1; i < mas.Length; i++)
+                if (mas[i - 1] == mas[i])
+                    count++;
+
+            Console.WriteLine($"В массиве {count} одинаковых пар элементов!");
+        }
+    }
+}
