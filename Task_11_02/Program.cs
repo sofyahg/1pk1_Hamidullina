f2907ed30979ec4fc0796fe11ae0d2603239997d
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-03-05 00:11:40 +0500

    Вторая задача 10 лекции выполнена

---
+++ Task_11_02/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_11_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Передача по ссылке (ref): Напишите метод, который принимает два целых числа
+             * по ссылке и меняет их местами. Проверьте, 
+             * изменились ли значения переменных вне метода*/
+
+            Console.Write("Введите число a >> ");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите число b >> ");
+
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            SwapNums(ref a, ref b);
+            Console.WriteLine($"\na = {a}; b = {b}");
+        }
+
+        /// <summary>
+        /// меняет числа местами
+        /// </summary>
+        /// <param name="a"> первое число </param>
+        /// <param name="b"> второе число </param>
+        static public void SwapNums(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
