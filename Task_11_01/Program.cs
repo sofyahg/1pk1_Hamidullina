45748162d8e9acb9d103e2d267d71932aa0eebec
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-03-05 00:06:24 +0500

    Выполнена 1 задача 11 темы

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_11_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Передача по значению: Напишите метод, который принимает два целых числа и меняет их
+             местами. Проверьте, изменились ли значения переменных вне метода*/
+
+
+            Console.Write("Введите число a >> ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("И число b >> ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            SwapNums(a, b);
+            Console.WriteLine($"\na = {a}; b = {b}");
+        }
+
+        /// <summary>
+        /// меняет числа местами
+        /// </summary>
+        /// <param name="a"> первое число </param>
+        /// <param name="b"> второе число </param>
+        static public void SwapNums(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+    }
+
