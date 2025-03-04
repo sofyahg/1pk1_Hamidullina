66890ca71108de37d15d5cffd0925967cc737211
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-03-04 23:46:21 +0500

    Выполнено 6 задание 10 темы

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        /* Создать Метод ArrayGeneration не возвращающий значения, принимает целое число n,
+         * выводит наконсоль сгенерированный массив размерности n*n.*/
+        {
+            void ArrayGeneration(int n)
+            {
+                int[,] array = new int[n, n];
+                Random rand = new Random();
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        array[i, j] = rand.Next(1, 100);      // Генерация случайных чисел от 1 до 99
+                        Console.Write(array[i, j] + "t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+                // Пример вызова метода
+            ArrayGeneration(3);
+        }
+    }
+}
