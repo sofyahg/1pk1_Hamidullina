8ca01a573cf2ff8e4e1ec1da6a2ee194fefbcb19
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-03-05 00:17:43 +0500

    Третье задание 11 лекции готово

---
+++ Task_11_03/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task_11_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Выходной параметр (out): Напишите метод, который принимает строку и возвращает через
+выходной параметр количество гласных и согласных букв в этой строке*/
+
+            Console.WriteLine("Введите строчку >> ");
+            string str = Console.ReadLine();
+
+            int counta;       //количество гласных букв
+            int countb;  //количество согласных букв
+
+            CountVowAndCons(str, out counta, out countb);
+
+            Console.WriteLine($"Количество гласных букв в строке: {counta}\nСогласных: {countb}");
+        }
+
+        /// <summary>
+        /// определяет количество гласных и согласных в строке
+        /// </summary>
+        /// <param name="str"> строка </param>
+        /// <param name="counta"> количество гласных </param>
+        /// <param name="countb"> количество согласных </param>
+        static void CountVowAndCons(string str, out int countVowel, out int countConsonant)
+        {
+            countVowel = 0;
+            countConsonant = 0;
+
+            str = str.ToLower(); //преобразовываем строки в низший регистр
+
+            char[] vowels = { 'a', 'e', 'y', 'u', 'i', 'o' };  //массив гласных букв
+            // подсчёт гласных букв в строке
+            foreach (char c in str)
+                foreach (char vow in vowels)
+                    if (vow == c)
+                    {
+                        countVowel++;
+                        break;
+                    }
+
+            countConsonant = str.Length - countVowel; // подсчёт согласных букв
+        }
+    }
+}
