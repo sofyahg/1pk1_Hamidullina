0d7d23adba1b8b942044eacd7b070796dcdbc153
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-03-05 07:54:45 +0500

    Пятое задание 11 лекции готово!

---
+++ Task_11_05/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_11_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Комбинирование ref и out: Напишите метод, который принимает два числа по ссылке(ref) и
+             * возвращает их сумму и произведение через выходные параметры(out).*/
+
+            Console.Write("Введите число a: ");
+
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число b: ");
+
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double sum, multy;  // сумма, произведение
+
+            SummAndMulty(ref a, ref b, out sum, out multy);
+
+            Console.WriteLine($"Сумма чисел: {sum}\nПроизведение чисел: {Math.Round(multy, 2)}");
+        }
+
+        /// <summary>
+        /// вычисление суммы и произведения двух чисел
+        /// </summary>
+        /// <param name="a"> первое число по ссылке </param>
+        /// <param name="b"> второе число по ссылке </param>
+        /// <param name="sum"> выходная суммма </param>
+        /// <param name="multy"> выходное произведение </param>
+        static public void SummAndMulty(ref double a, ref double b, out double sum, out double multy)
+        {
+            sum = a + b;
+            multy = a * b;
+        }
+    }
+    
+}
