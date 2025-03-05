d22c01e9d34a49e23dd7167f17bcf6e08e5b8127
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-03-05 07:59:18 +0500

    7 задание 11 лекции выполнено!

--- Task_11_06/Program.cs
+++ Task_11_06/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* Передача массива по значению: Напишите метод, 
+             * который принимает массив целых чисел и
+             * массив вне метода*/
+
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Исходный массив:");
+            int[] nums = new int[rnd.Next(10, 20)];    //массив
+            //заполняем и выводим
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rnd.Next(1, 10);
+                Console.Write($"{nums[i]} ");
+            }
+
+            IncrementList(nums);
+
+            Console.WriteLine("\nРезультирующий массив:");
+            foreach (int i in nums)
+                Console.Write($"{i} ");
+        }
+
+        /// <summary>
+        /// увеличивает елементы массива на 1
+        /// </summary>
+        /// <param name="nums"> массив </param>
+        static public void IncrementList(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                nums[i]++;
         }
     }
 }
