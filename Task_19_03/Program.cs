39bde46761d4fd139e01aa67623f90c65e302b4c
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-04-07 23:40:39 +0500

    3 задание 19 лекции готово!

--- Task_19_03/Program.cs
+++ Task_19_03/Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
+
+        /* Напишите консольное приложение, в котором осуществляется построчный пользовательский
+            ввод (ввод каждой строки подтверждается нажатием на enter). количество введенных строк
+            произвольно, ввод завершается при вводе пустой строки.
+            После окончания ввода произведите объединение всех ранеее введенных строк в одну с
+            использованием разделителя «-»
+            Например:
+            Введите строку 1: "Hello"
+            Введите строку 2: "world"
+            Введите строку 3: "C#"
+            Результат: "Hello-world-C#"*/
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> line = new List<string>();
+
+            Console.WriteLine("Введите строки:");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                // Прервём цикл
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+
+                line.Add(input);
+            }
+
+            string result = string.Join("-", line);
+
+            Console.WriteLine($"Результат: {result}");
         }
     }
 }
