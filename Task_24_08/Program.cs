642b2196a67255295683046929d6e96ab6163326
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-04-08 07:15:27 +0500

    8 задача 24 лекции готова

--- Task_24_08/Program.cs
+++ Task_24_08/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+       
+          /*реализуйте функцию, осуществляющую поиск текста в файле 
+        * и его замену на значения, введенные пользователем
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "text.txt";
+
+            Console.Write("Введите текст для поиска: ");
+            string search = Console.ReadLine();
+
+            Console.Write("Введите текст для замены: ");
+            string replacement = Console.ReadLine();
+
+            //Чтение содержимого
+            string fileContent = File.ReadAllText(path);
+
+            //Выполняем замену
+            string update = fileContent.Replace(search, replacement);
+
+            //Сохранение изменений
+            File.WriteAllText(path, update);
         }
     }
 }
