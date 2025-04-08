000939dd1a618bdf937dd0dee85800b506e10e2c
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-04-08 06:46:55 +0500

    6 задание 20 лекции готово!

--- Task_20_06/Program.cs
+++ Task_20_06/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+
+
+        /*Создайте программу, имитирующую работу светофора, используя перечисление TrafficLightColor:
+       • Red
+       • Yellow
+       • Green
+       Реализуйте автоматическое переключение цветов (каждые 3 секунды). 
+       При смене цвета выводите его в консоль (можно с задержкой Thread.Sleep). 
+       Добавьте возможность ручного переключения (например, по нажатию клавиши).
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TrafficLight trLight = new TrafficLight();
+
+            //Автоматическое переключение в отдельном потоке
+            Thread automatThread = new Thread(trLight.Automatic);
+            automatThread.Start();
+
+            //Ручное переключение
+            trLight.Switch();
         }
     }
 }
---
+++ Task_20_06/TrafficLight.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_06
+{
+    internal class TrafficLight
+    {
+        private TrafficLightColor currentColor;
+
+
+        public TrafficLight()
+        {
+            currentColor = TrafficLightColor.Red; // Изначальный цвет
+        }
+
+        public void Automatic()
+        {
+            while (true)
+            {
+                switch (currentColor)
+                {
+                    case TrafficLightColor.Red:
+                        SetColor(TrafficLightColor.Green);
+                        break;
+                    case TrafficLightColor.Green:
+                        SetColor(TrafficLightColor.Yellow);
+                        break;
+                    case TrafficLightColor.Yellow:
+                        SetColor(TrafficLightColor.Red);
+                        break;
+                }
+
+                Thread.Sleep(3000); //Задержка 3 секунды
+            }
+        }
+
+        public void SetColor(TrafficLightColor color)
+        {
+            currentColor = color;
+            Console.Clear();
+            Console.WriteLine($"Текущий цвет светофора: {currentColor}");
+
+        }
+
+        public void Switch()
+        {
+            while (true)
+            {
+                var input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Spacebar) // Переключение по пробелу
+                {
+                    switch (currentColor)
+                    {
+                        case TrafficLightColor.Red:
+                            SetColor(TrafficLightColor.Green);
+                            break;
+                        case TrafficLightColor.Green:
+                            SetColor(TrafficLightColor.Yellow);
+                            break;
+                        case TrafficLightColor.Yellow:
+                            SetColor(TrafficLightColor.Red);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
