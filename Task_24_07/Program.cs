90c77b72ba1da71fdddd75ec3ba94923dd8f035e
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-04-08 07:10:51 +0500

    7 заадача 24 лекции решена!

--- Task_24_07/Program.cs
+++ Task_24_07/Program.cs
@@ -2,9 +2,36 @@
 {
     internal class Program
     {
+
+         /*Реализуйте функцию, которая ищет заданное слово в текстовом файле 
+        * и возвращает все строки, содержащиеэтослово (регистронезависимо).
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "text.txt";
+            string word = "опыт";
+
+            List<string> linesWord = FindWord(path, word);
+
+            //Выводим результаты
+            foreach (string line in linesWord)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static List<string> FindWord(string path, string word)
+        {
+            List<string> result = new List<string>();
+
+
+            foreach (string line in File.ReadLines(path))
+            {
+                result.Add(line);
+            }
+
+            return result;
         }
     }
 }
+
