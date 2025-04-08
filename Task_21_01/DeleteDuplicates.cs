316c91241f7545023cb91fb82f256b3dabefd8ce
СОФЬЯ Хамидуллина <hamidullinasofa559@hotmail.com>
2025-04-08 06:53:40 +0500

    1 задание 21 лекции выполнено!

---
+++ Task_21_01/DeleteDuplicates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_01
+{
+    internal class DeleteDuplicates
+    {
+
+
+        public static void RemoveDuplicates(List<int> list)
+        {
+            HashSet<int> hash = new(list);
+            List<int> newNumbers = new(hash);
+
+            for (int i = 0; i < list.Count;)
+            {
+                if (i >= newNumbers.Count) list.RemoveAt(i);
+                else
+                {
+                    list[i] = newNumbers[i];
+                    i++;
+                }
+            }
+        }
+    }
+}
--- Task_21_01/Program.cs
+++ Task_21_01/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+
+        /*Написать метод, который удаляет повторяющиеся элементы из списка, сохраняя порядок.
+         */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> numbrs = new List<int> { 1, 5, 1, 3, 3, 7, 12, 75, 43, 63, 43, 9, 4 };
+            Console.WriteLine("Список до:");
+            foreach (int n in numbrs)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Список после: ");
+            DeleteDuplicates.RemoveDuplicates(numbrs);
+            foreach (int i in numbrs)
+                Console.Write(i + " ");
         }
     }
 }
